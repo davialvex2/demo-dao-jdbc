490eb4b4a3eb3281b76c1f80a2a11b06b51e9549
Davi Augusto <davi.miranda0919@gmail.com>
2024-11-25 16:29:08 -0300

    Alteração classe Vendedor

--- src/entidades/Vendedor.java
+++ src/entidades/Vendedor.java
@@ -4,10 +4,116 @@
  */
 package entidades;
 
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
 /**
  *
  * @author Computador
  */
-public class Vendedor {
+public class Vendedor implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    private Integer id;
+    private String nome;
+    private String email;
+    private Date birthDate;
+    private Double salarioBase;
+    
+    private Departamento departamento;
+
+    public Vendedor() {
+    }
+    
+    
+    public Vendedor(Integer id, String nome, String email, Date birthDate, Double salarioBase, Departamento departamento) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.salarioBase = salarioBase;
+        this.departamento = departamento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(Double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vendedor other = (Vendedor) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor{" + "id=" + id + ", nome=" + nome + ", email=" + email + ", birthDate=" + birthDate + ", salarioBase=" + salarioBase + ", departamento=" + departamento + '}';
+    }
+
+ 
+    
     
 }

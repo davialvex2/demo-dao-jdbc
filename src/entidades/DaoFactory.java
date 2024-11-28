39750a16397f8342a0efcf4b7909247b97ee42f0
Davi Augusto <davi.miranda0919@gmail.com>
2024-11-28 11:11:38 -0300

    Criando classe VendedorDaoJDBC e DaoFactory

---
+++ src/entidades/DaoFactory.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import model.dao.VendedorDao;
+import model.dao.jdbc.VendedorDaoJDBC;
+
+/**
+ *
+ * @author Computador
+ */
+public class DaoFactory {
+    
+    public static VendedorDao criarVendedorDao(){
+        return new VendedorDaoJDBC();
+    }
+    
+}
---
+++ src/model/dao/jdbc/VendedorDaoJDBC.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.dao.jdbc;
+
+import entidades.Vendedor;
+import java.util.List;
+import model.dao.VendedorDao;
+
+/**
+ *
+ * @author Computador
+ */
+public class VendedorDaoJDBC implements VendedorDao{
+
+    @Override
+    public void inserir(Vendedor obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void atualizar(Vendedor obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void excluirId(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Vendedor buscarId(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Vendedor> buscarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
